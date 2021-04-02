5ebc16b19fa7f75ad419f5241486f8822f04799a
Max Maischein <corion@corion.net>
2021-04-02 20:16:48 +0200

    Delegate unknown method calls to Net::Protocol::OBSRemote

--- bin/login.pl
+++ bin/login.pl
@@ -1,4 +1,5 @@
 #!perl
+use 5.012;
 use strict;
 use warnings;
 use Filter::signatures;
@@ -17,20 +18,23 @@ GetOptions(
 
 $url //= 'ws://localhost:4444';
 
-my $h = Mojo::OBS::Client->new;
-my $r = $h->connect($url)->then(sub {
-    $h->send_message($h->protocol->GetVersion());
-})->then(sub {
-    $h->send_message($h->protocol->GetAuthRequired());
-})->then(sub( $challenge ) {
-    $h->send_message($h->protocol->Authenticate($password,$challenge));
-})->then(sub( $challenge ) {
-    $h->send_message($h->protocol->SetTextFreetype2Properties( source => 'Text.NextTalk',text => 'Hello World'))
-})->then(sub( $challenge ) {
-    $h->send_message($h->protocol->GetSourceSettings( sourceName => 'VLC.Vortrag', sourceType => 'vlc_source'))
-})->then(sub( $challenge ) {
+my $h = Mojo::OBS::Client->new(
+    #debug => 1
+);
+
+my $r = $h->login( $url, $password )
+->then(sub($res) {
+    if( $res->{error}) {
+        die $res->{error}
+    } else {
+        say "Logged into $url";
+        $h->SetTextFreetype2Properties( source => 'Text.NextTalk',text => 'Hello World')
+    };
+})->then(sub(@) {
+    $h->GetSourceSettings( sourceName => 'VLC.Vortrag', sourceType => 'vlc_source')
+})->then(sub(@) {
     # Queue up a talk
-    $h->send_message($h->protocol->SetSourceSettings( sourceName => 'VLC.Vortrag', sourceType => 'vlc_source',
+    $h->SetSourceSettings( sourceName => 'VLC.Vortrag', sourceType => 'vlc_source',
     sourceSettings => {
                                 'playlist' => [
                                                 {
@@ -40,11 +44,15 @@ my $r = $h->connect($url)->then(sub {
                                                 }
                                               ]
                                           },
-    ))
+    );
+
+    say "Stopping loop";
+    Mojo::IOLoop->stop_gracefully;
+    return Future->done;
+
 })->catch(sub {
-    use Data::Dumper;
-    warn Dumper \@_;
-    });
+    warn $_[0];
+    Mojo::IOLoop->stop_gracefully;
+});
 
 Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
-
--- lib/Mojo/OBS/Client.pm
+++ lib/Mojo/OBS/Client.pm
@@ -14,6 +14,8 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 with 'Moo::Role::RequestReplyHandler';
 
+use Carp 'croak';
+
 =head1 NAME
 
 Mojo::OBS::Client - Mojolicious client for the OBS WebSocket remote plugin
@@ -38,9 +40,13 @@ Mojo::OBS::Client - Mojolicious client for the OBS WebSocket remote plugin
 
 =cut
 
-sub future($self, $loop=$self->ioloop) {
-    Future::Mojo->new( $loop )
-}
+=head1 ACCESSORS
+
+=head2 C<< ->ioloop >>
+
+Access the underlying L<Mojo::IOLoop>
+
+=cut
 
 has ioloop => (
     is => 'ro',
@@ -49,6 +55,12 @@ has ioloop => (
     },
 );
 
+=head2 C<< ->ua >>
+
+Access the L<Mojo::UserAgent> object used to talk to OBS.
+
+=cut
+
 has ua => (
     is => 'ro',
     default => sub {
@@ -56,9 +68,21 @@ has ua => (
     },
 );
 
+=head2 C<< ->tx >>
+
+The websocket connection to OBS.
+
+=cut
+
 has tx => (
     is => 'rw',
 );
+
+=head2 C<< ->protocol >>
+
+The L<Net::Protocol::OBSRemote> instance used to generate the OBS messages.
+
+=cut
 
 has protocol => (
     is => 'ro',
@@ -66,6 +90,28 @@ has protocol => (
         return Net::Protocol::OBSRemote->new();
     },
 );
+
+=head2 C<< ->debug >>
+
+Switch on debug messages to STDERR. Also enabled if
+C<< $ENV{PERL_MOJO_OBS_CLIENT_DEBUG} >> is set to a true value.
+
+=cut
+
+has debug => (
+    is => 'ro',
+    default => sub {
+        return !!$ENV{PERL_MOJO_OBS_CLIENT_DEBUG};
+    },
+);
+
+=head1 METHODS
+
+=cut
+
+sub future($self, $loop=$self->ioloop) {
+    Future::Mojo->new( $loop )
+}
 
 sub get_reply_key($self,$msg) {
     $msg->{'message-id'}
@@ -99,12 +145,16 @@ sub connect($self,$ws_url) {
                 my $payload = decode_json(encode('UTF-8',decode('Latin-1', $msg)));
 
                 if( my $type = $payload->{"update-type"}) {
-                    #use Data::Dumper;
-                    #warn '***' . Dumper $payload;
+                    if( $self->debug ) {
+                        require Data::Dumper;
+                        say "*** " . Data::Dumper::Dumper( $msg );
+                    };
                     $self->event_received( $type, $payload );
                 } elsif( my $id = $self->get_reply_key( $payload )) {
-                    #use Data::Dumper;
-                    #warn '<==' . Dumper $payload;
+                    if( $self->debug ) {
+                        require Data::Dumper;
+                        say "<== " . Data::Dumper::Dumper( $msg );
+                    };
                     $self->message_received($payload);
                 };
             });
@@ -121,13 +171,59 @@ sub shutdown( $self ) {
 
 sub send_message($self, $msg) {
     my $res = $self->future();
-    #use Data::Dumper; say "==>" . Dumper $msg;
+
+    if( $self->debug ) {
+        require Data::Dumper;
+        say "==> " . Data::Dumper::Dumper( $msg );
+    };
     my $id = $msg->{'message-id'};
     $self->on_message( $id, sub($response) {
         $res->done($response);
     });
     $self->tx->send( encode_json( $msg ));
     return $res
+};
+
+=head1 METHODS
+
+For the OBS methods, see L<Net::Protocl::OBSRemote>.
+
+=cut
+
+# We delegate all unknown methods to $self->protocol
+sub AUTOLOAD( $self, @args ) {
+    our $AUTOLOAD =~ /::(\w+)$/
+        or croak "Weird AUTOLOAD method '$AUTOLOAD'";
+    return if $1 eq 'DESTROY';
+    my $method = $self->protocol->can("$1")
+        or croak "Unknown OBS method '$1'";
+
+    my $payload = $method->($self->protocol, @args);
+    return $self->send_message( $payload );
+}
+
+=head2 C<< ->login $url, $password >>
+
+    $obs->login('ws://localhost:4444', 'secret')
+    ->then(sub( $res ){
+        if( $res->{error} ) {
+            warn $res->{error};
+            return
+        };
+    })
+
+Performs the login authentication with the OBS websocket
+
+=cut
+
+sub login( $h, $url, $password ) {
+    return $h->connect($url)->then(sub {
+        $h->GetVersion();
+    })->then(sub {
+        $h->GetAuthRequired();
+    })->then(sub( $challenge ) {
+        $h->Authenticate($password,$challenge);
+    });
 };
 
 1;
